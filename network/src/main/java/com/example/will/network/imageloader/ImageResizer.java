ee2ba39d9bd524a3992cc3a4f350916671636a7c
will <Awille@qq.com>
2019-03-12 18:14:50 +0800

    根据bitmapfactory的injustdecodebounds来先获得图片尺寸，然后对图片进行压缩

--- network/src/main/java/com/example/will/network/imageloader/ImageResizer.java
+++ network/src/main/java/com/example/will/network/imageloader/ImageResizer.java
@@ -5,15 +5,36 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 
+import java.io.FileDescriptor;
+
 public class ImageResizer {
     private static final String TAG = "ImageResizer";
 
     public Bitmap decodeSampleBitmapFromResource(Resources resources, int resId, int reqWidth, int reqHeight) {
-        final BitmapFactory.Options options = new BitmapFactory.Options();
+        BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
-        return null;
+        BitmapFactory.decodeResource(resources, resId, options);
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(resources, resId, options);
     }
 
+    public Bitmap decodeSampleBitmapFromFileDescriptor(FileDescriptor fd, int reqWidht, int reqHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFileDescriptor(fd, null, options);
+        options.inSampleSize = calculateInSampleSize(options, reqWidht, reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFileDescriptor(fd, null, options);
+    }
+
+    /**
+     * 计算图片压缩比例
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @return 图片压缩倍数
+     */
     public int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
         final int height = options.outHeight;
         final int width = options.outWidth;
@@ -25,8 +46,14 @@ public class ImageResizer {
         if (width > reqWidth || height > reqHeight) {
             int halfWidth = width / 2;
             int halfHeight = height / 2;
-
+            inSampleSize = 2;
+            while (halfHeight > reqHeight && halfWidth > reqWidth) {
+                halfHeight /= 2;
+                halfWidth /= 2;
+                inSampleSize *= 2;
+            }
         }
+        Log.e(TAG, "the inSampleSize =" + inSampleSize);
         return inSampleSize;
     }
 }
